90f8119b963d1b52b004f59104a8c03b5a3d7c92
ekkx <geniusnaru@gmail.com>
2025-05-15 13:27:42 +0000

    refactor: extracting article data

--- gigazine/utils.py
+++ gigazine/utils.py
@@ -3,22 +3,17 @@ import discord
 
 from models import GigazineArticle
 
-def get_article_url(text: str):
-    url_regex = r"(?P<url>https?://[^\s]+)"
-    match = re.search(url_regex, text)
-    if match:
-        return match.group("url")
-    else:
-        return ""
-
-def get_article_title(text:str, url:str):
-    if url is not None:
-        return re.sub(url, '', text)
-    return text
-
 def extract_article(message: discord.Message) -> GigazineArticle:
-  url = get_article_url(message.content)
+  url_pattern = r"https?://[^\s]+"
+  match = re.search(url_pattern, message.content)
+  if match:
+    url = match.group()
+    title = message.content.replace(url, "").strip()
+    return GigazineArticle(
+      title=title,
+      url=url
+    )
   return GigazineArticle(
-    title=get_article_title(message.content, url),
-    url=url
+    title=message.content,
+    url=""
   )
